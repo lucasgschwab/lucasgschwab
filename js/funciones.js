f88a82b50f6b69439a53cc59989c71ca5da27a43
lucasgschwab <schwab.lucas418@gmail.com>
2023-02-28 23:29:38 -0300

    v1

---
+++ js/funciones.js
@@ -0,0 +1,12 @@
+
+function mensaje(texto){
+    alert(texto);
+}
+
+function linkedin() {
+    mensaje("vas a salir del sitio de Lucas Gustavo Schwab a linkedin");
+    window.open("https://www.linkedin.com");
+  }
+  
+
+  
